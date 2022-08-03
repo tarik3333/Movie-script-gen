fcda4e814fe0ab68170fc9a59429ddad1dceaef6
aliswagmaster <ac8pn@virginia.edu>
2022-08-03 09:48:17 -0400

    bard: Reading shakespearean sonnets and learning how to write an original one using tensorflow

--- movielist.py
+++ movielist.py
@@ -1,15 +1,26 @@
-liste = ['Independence-Day', 'Innerspace', 'Interstellar', 'Island,-The', 'Jason-X', 'Judge-Dredd', 'Jurassic-Park', 'Jurassic-Park-III', 'Jurassic-Park:-The-Lost-World', 'Leviathan', 'Limitless', 'Logan', "Logan's-Run", 'Looper', 'Lost-Horizon', 'Lost-in-Space', 'Mad-Max-2:-The-Road-Warrior', 'Manchurian-Candidate,-The', 'Martian,-The', 'Matrix-Reloaded,-The', 'Matrix,-The', 'Megamind', 'Men-in-Black', 'Men-in-Black-3', 'Mimic', 'Minority-Report', 'Mission-to-Mars', 'Moon', 'Next', 'Oblivion', 'Omega-Man', 'Pandorum', 'Passengers', 'Paul', 'Perfect-Creature', 'Pi', 'Pitch-Black', 'Planet-of-the-Apes,-The', 'Postman,-The', 'Predator', 'Prestige,-The', 'Priest', 'Prometheus', 'Red-Planet', 'Repo-Man', 'Resident-Evil', 'Return-of-the-Apes', 'Rise-of-the-Planet-of-the-Apes', 'Robocop', 'Rocky-Horror-Picture-Show,-The', 'S.-Darko', 'Scary-Movie-2', 'Serenity', 'Shivers', 'Signs', 'Simone', 'Slither', 'Solaris', 'Soldier', 'Source-Code', 'Space-Milkshake', 'Sphere', 'Spider-Man', 'Star-Trek', 'Star-Trek-II:-The-Wrath-of-Khan', 'Star-Trek:-First-Contact', 'Star-Trek:-Generations', 'Star-Trek:-Nemesis', 'Star-Trek:-The-Motion-Picture', 'Star-Wars:-A-New-Hope', 'Star-Wars:-Attack-of-the-Clones', 'Star-Wars:-Return-of-the-Jedi', 'Star-Wars:-Revenge-of-the-Sith', 'Star-Wars:-The-Empire-Strikes-Back', 'Star-Wars:-The-Phantom-Menace', 'Starman', 'Starship-Troopers', 'Strange-Days', 'Super-8', 'Supergirl', 'Surrogates', 'Terminator', 'Terminator-2:-Judgement-Day', 'Terminator-Salvation', 'The-Rage:-Carrie-2', 'Thing,-The', 'Time-Machine,-The', 'Total-Recall', 'Transformers:-The-Movie', 'Tremors', 'TRON', 'TRON:-Legacy', 'Truman-Show,-The', 'Twins', 'V-for-Vendetta', 'Vanilla-Sky', 'Virtuosity', 'Wall-E', 'War-of-the-Worlds', 'Watchmen', 'Wild-Wild-West', 'X-Files:-Fight-the-Future,-The', 'X-Men']
 
-#liste1 = []
-#for i in range(len(liste)):
-#    liste2 = ""
-#    for j in liste[i]:
-#        if j == " ":
-#            liste2 = liste2 + "-"
-#        if j != " ":
-#            liste2 = liste2 + j
-#            
-#    liste1.append(liste2)
+liste = []
 
-#print(liste1)
+def findRoman(number):
+    num = [1, 4, 5, 9, 10, 40, 50, 90,
+        100, 400, 500, 900, 1000]
+    sym = ["I", "IV", "V", "IX", "X", "XL",
+        "L", "XC", "C", "CD", "D", "CM", "M"]
+    i = 12
+    result = ""
+      
+    while number:
+        div = number // num[i]
+        number %= num[i]
+
+        while div:
+            result += sym[i]
+            div -= 1
+        i -= 1
+    return result
+
+for number in range(1, 155):
+    liste.append(findRoman(number))
+
+print(liste)
                
--- rpdeneme.py
+++ rpdeneme.py
@@ -3,21 +3,22 @@ import time
 import pickle
 from movielist import liste
 import requests
+from selenium.webdriver.common.by import By
 
-#print(liste)
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='/Users/alicheraghpour/Desktop/scriptwriting/chromedriver')
 
 for i in range(len(liste)):
-    path = "https://imsdb.com/scripts/{}.html".format(liste[i])
+    path = "http://shakespeare.mit.edu/Poetry/sonnet.{}.html".format(liste[i])
+    # print(path)
     req = requests.get(path)
     if req.status_code == requests.codes['ok']:
         print(liste[i])
         driver.get(path)
-        path1 = "/html/body/table[2]/tbody/tr/td[3]/table/tbody/tr/td/pre"
+        path1 = "/html/body/blockquote"
         dos = open("scripts.txt","a")
         time.sleep(1)
         try:
-            dos.write(driver.find_element_by_xpath(path1).text)
+            dos.write(driver.find_element(By.XPATH, path1).text)
             print(liste[i],"completed")
         except UnicodeEncodeError:
             pass
